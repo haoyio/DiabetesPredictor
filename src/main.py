b19a986826e31f7518cbfb0ed27f33a6b819d9ac
haoyio <haoyi@stanford.edu>
2014-11-07 15:52:13 -0800

    outlined main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 
+# read in files and transform into formatted data we want to work with
+
+# run naive bayes classifier for data
+
+# run structure learning for Bayesian network (BN)
+  # define scoring function (read: reward function) for local search heuristic
+  # run search heuristics:
+    # local search (pure greedy)
+    # local search with tabu list
+    # beam search
+    # K2 search
+  # manually look at BN and change structure to fit intuition
+
+# use inference techniques to get conditional probabilities
+
+# check error rates, shazaam
