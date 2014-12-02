abbb8f4c2b999a8a087b6127d7bbc7629604dfdf
XiaoSong Mu <xiaosongmu@gmail.com>
2014-12-01 20:30:30 -0800

    laplace smoothing

--- src/paramLearn/paramLearn.py
+++ src/paramLearn/paramLearn.py
@@ -2,7 +2,35 @@ import sys
 from probabilityTable import *
 
 class ParamLearn:
-  
+  self.domains = {}
+  self.domains["state"] = ["AK","AL","AR","AZ","CA","CO","CT","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"]
+  self.domains["gender"] = ["M","F"]
+  self.domains["age"] = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+  self.domains["height"] = range(40,90)
+  self.domains["weight"] = range(0,490,10)
+  self.domains["BMI"] = range(0, 170)
+  self.domains["sBP"] = range(10, 225, 5)
+  self.domains["dBP"] = range(0, 150, 5)
+  self.domains["diabetes"] = [0,1]
+  self.domains["code001139"] = [0,1]
+  self.domains["code140239"] = [0,1]
+  self.domains["code240279"] = [0,1]
+  self.domains["code280289"] = [0,1]
+  self.domains["code290319"] = [0,1]
+  self.domains["code320359"] = [0,1]
+  self.domains["code360389"] = [0,1]
+  self.domains["code390459"] = [0,1]
+  self.domains["code460519"] = [0,1]
+  self.domains["code520579"] = [0,1]
+  self.domains["code580629"] = [0,1]
+  self.domains["code630679"] = [0,1]
+  self.domains["code680709"] = [0,1]
+  self.domains["code710739"] = [0,1]
+  self.domains["code740759"] = [0,1]
+  self.domains["code760779"] = [0,1]
+  self.domains["code780799"] = [0,1]
+  self.domains["code800999"] = [0,1] 
+
   # countFile = e.g. train2.csv 
   def __init__(self, countFile):
     self.pTable = PTable(countFile)
@@ -10,25 +38,22 @@ class ParamLearn:
   def getParentChildJointProb(self, node_id, node_val, parentDict, childDict):
     
     jointParams = dict(parentDict.items()+childDict.items())
-    jointCount = self.pTable.getCounts(jointParams)
+    # jointCount = self.pTable.getCounts(jointParams)
+    # #posteriorCount
+    # jointParams[node_id] = node_val
+    # postCount = self.pTable.getCounts(jointParams)
+    # return postCount*1.0 / (jointCount+0.0000000001)  
+    return getParentJointProb(node_id, node_val, jointParams)
 
-    #posteriorCount
-    jointParams[node_id] = node_val
-    postCount = self.pTable.getCounts(jointParams)
-
-    return postCount*1.0 / (jointCount+0.0000000001)  
-
-  def getParentJointProb(self, node_id, node_val, parentDict):
+  def getParentJointProb(self, node_id, node_val, params):
      
     #parentCount
-    params = parentDict
     parentCount = self.pTable.getCounts(params)
     #posteriorCount
-    params = parentDict
     params[node_id] = node_val
     postCount = self.pTable.getCounts(params)
 
-    return postCount*1.0 / (parentCount+0.0000000001)
+    return (postCount+1) / (parentCount+len(self.domains[node_id]))
 
 def main(argv):
   countFile = argv[1]
