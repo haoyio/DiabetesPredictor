66d41444fe8f26ce4667121d4da97715511fb45f
haoyio <haoyi@stanford.edu>
2014-12-01 17:18:32 -0800

    meh inference

--- src/GibbsInference.py
+++ src/GibbsInference.py
@@ -104,10 +104,9 @@ for row in range(nSamples):
   # TODO: we need to have some convergence criteria; too slow!
   for iterIdx in range(iterNum):
     withheld = withheldData[row]
-    newWithheld = {}
     
     # loop through labels in withheldData[row] and random sample 
-    # based on weights  form newWithheld
+    # based on weights
     for name in withheldData[row]:
       
       jProbs = []
@@ -131,11 +130,8 @@ for row in range(nSamples):
                                               parentValues, childrenValues))
       
       # assign value to unknown variable via weighted random sampling
-      newWithheld[name] = weightedChoice(domains[name], jProbs)
+      withheldData[row][name] = weightedChoice(domains[name], jProbs)
 
-    # set unknown variables to newly sampled values
-    withheldData[row] = newWithheld
-  
   # record inferred value for current row 
   parentValues = {parent.id : data[row][nameMap[parent.id]] \
                         for parent in graph.getNode("diabetes").getParents()}
