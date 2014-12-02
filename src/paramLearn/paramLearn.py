f9a15f5843bbe74a1f66f5c96e4ca088ab509162
XiaoSong Mu <xiaosongmu@gmail.com>
2014-12-01 20:43:16 -0800

    fixed ParamLearn

--- src/paramLearn/paramLearn.py
+++ src/paramLearn/paramLearn.py
@@ -1,39 +1,39 @@
 import sys
 from probabilityTable import *
 
-class ParamLearn:
-  self.domains = {}
-  self.domains["state"] = ["AK","AL","AR","AZ","CA","CO","CT","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"]
-  self.domains["gender"] = ["M","F"]
-  self.domains["age"] = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
-  self.domains["height"] = range(40,90)
-  self.domains["weight"] = range(0,490,10)
-  self.domains["BMI"] = range(0, 170)
-  self.domains["sBP"] = range(10, 225, 5)
-  self.domains["dBP"] = range(0, 150, 5)
-  self.domains["diabetes"] = [0,1]
-  self.domains["code001139"] = [0,1]
-  self.domains["code140239"] = [0,1]
-  self.domains["code240279"] = [0,1]
-  self.domains["code280289"] = [0,1]
-  self.domains["code290319"] = [0,1]
-  self.domains["code320359"] = [0,1]
-  self.domains["code360389"] = [0,1]
-  self.domains["code390459"] = [0,1]
-  self.domains["code460519"] = [0,1]
-  self.domains["code520579"] = [0,1]
-  self.domains["code580629"] = [0,1]
-  self.domains["code630679"] = [0,1]
-  self.domains["code680709"] = [0,1]
-  self.domains["code710739"] = [0,1]
-  self.domains["code740759"] = [0,1]
-  self.domains["code760779"] = [0,1]
-  self.domains["code780799"] = [0,1]
-  self.domains["code800999"] = [0,1] 
+class ParamLearn: 
 
   # countFile = e.g. train2.csv 
   def __init__(self, countFile):
     self.pTable = PTable(countFile)
+    self.domains = {}
+    self.domains["state"] = ["AK","AL","AR","AZ","CA","CO","CT","DE","FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"]
+    self.domains["gender"] = ["M","F"]
+    self.domains["age"] = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95]
+    self.domains["height"] = range(40,90)
+    self.domains["weight"] = range(0,490,10)
+    self.domains["BMI"] = range(0, 170)
+    self.domains["sBP"] = range(10, 225, 5)
+    self.domains["dBP"] = range(0, 150, 5)
+    self.domains["diabetes"] = [0,1]
+    self.domains["code001139"] = [0,1]
+    self.domains["code140239"] = [0,1]
+    self.domains["code240279"] = [0,1]
+    self.domains["code280289"] = [0,1]
+    self.domains["code290319"] = [0,1]
+    self.domains["code320359"] = [0,1]
+    self.domains["code360389"] = [0,1]
+    self.domains["code390459"] = [0,1]
+    self.domains["code460519"] = [0,1]
+    self.domains["code520579"] = [0,1]
+    self.domains["code580629"] = [0,1]
+    self.domains["code630679"] = [0,1]
+    self.domains["code680709"] = [0,1]
+    self.domains["code710739"] = [0,1]
+    self.domains["code740759"] = [0,1]
+    self.domains["code760779"] = [0,1]
+    self.domains["code780799"] = [0,1]
+    self.domains["code800999"] = [0,1]
 
   def getParentChildJointProb(self, node_id, node_val, parentDict, childDict):
     
---
+++ src/paramLearn/test.py
@@ -0,0 +1,21 @@
+import unittest
+import paramLearn
+import tree
+
+class TestSubmission(unittest.TestCase):
+
+    def test_one(self):
+        tree = Tree('')
+        self.assertEquals(submission.extractWordFeatures("hello world"), {"hello":1, "world":1})
+        test1 = "A duck and a boy went to the super-duper-pond a fortnight ago."
+        test1_ans = {"A":1, "duck":1, "and":1, "a":2, "boy":1, "went": 1, "to":1, "the":1,\
+         "super-duper-pond":1, "fortnight":1, "ago.":1}
+        self.assertEquals(submission.extractWordFeatures(test1), test1_ans)
+        test2 = "I am what I am"
+        test2_ans = {"I":2, "am":2, "what":1}
+        self.assertEquals(submission.extractWordFeatures(test2), test2_ans)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
