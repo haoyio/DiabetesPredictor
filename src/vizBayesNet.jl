e33da8149b5bad4ef42a75fa8426816b813f0257
haoyio <haoyi@stanford.edu>
2014-12-02 04:46:46 -0800

    modded poster, added graph pdf

--- src/vizBayesNet.jl
+++ src/vizBayesNet.jl
@@ -4,9 +4,10 @@ using Graphs
 using TikzGraphs
 using TikzPictures
 
-dataset = readtable("train.csv")
+dataset = readtable("train2_translated.csv")
 
 for arg = 1:length(ARGS)
+  arg = 1
   inname = ARGS[arg]
   title = splitext(inname)[1]
   outname = title * ".pdf"
@@ -15,11 +16,13 @@ for arg = 1:length(ARGS)
   b.domains = [DiscreteDomain([x for x in unique(dataset[label])]) 
                for label in names(dataset)]
 
-  fin = open(title * ".gph", "r")
+  fin = open(inname, "r")
   lines = readlines(fin)
   close(fin)
-  for line in lines
-    nodes = split(line, ", ")
+  
+  for i = 1:length(lines)
+    line = lines[i]
+    nodes = split(line, ",")
     src = convert(Symbol, nodes[1])
     tgt = convert(Symbol, nodes[2][1:end-1])
     addEdge!(b, src, tgt)
@@ -28,4 +31,5 @@ for arg = 1:length(ARGS)
   save(b::BayesNet, filename::String) = TikzPictures.save(PDF(filename), TikzGraphs.plot(b.dag, ASCIIString[string(s) for s in b.names]))
   save(b, outname)
   @printf("Output saved to %s\n", outname)
+
 end # for arg
